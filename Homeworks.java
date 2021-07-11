c6391e9da938d91f800794421a2d4507566abc46
Samet Ibis <56191026+sametibis@users.noreply.github.com>
2021-07-11 12:36:51 -0600

    11.07.2021-Homeworks
    Arrays exercises.

---
+++ Homeworks.java
@@ -0,0 +1,57 @@
+package practiceOzkanAyata;
+
+import java.util.Arrays;
+
+public class Homeworks {
+
+	// 9. Write a method that accepts an Array of integers and returns the max.
+
+	// 10. Write a method that accepts an Array of integers and returns the second
+	// max.
+
+	// 11. Write a method that accepts an Array of ints returns the sorted Array in
+	// ascending order.Do not use the sort method.
+
+	public static void main(String[] args) {
+		int array[] = {189,-5, 87, 10, 20, 25, 63, 96, 57};
+		
+		System.out.println(findMax(array));
+		System.out.println(findSecondMax(array));
+		System.out.println(Arrays.toString(ascendingSorter(array)));
+
+	}
+	
+	
+	// 9
+	static int findMax(int[] arr) {
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+	
+	// 10
+	static int findSecondMax(int[] arr) {
+		Arrays.sort(arr);		
+		return arr[arr.length-2];
+	}
+	
+	// 11: Not use the sort() method
+	static int[] ascendingSorter(int[] arr) {
+		int temp = 0;
+		for(int i = 0; i<arr.length; i++) {
+			for(int j = i+1; j<arr.length; j++) {
+				if(arr[j] < arr[i]) {
+					temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+	
+}
